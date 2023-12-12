6328c902a74d5e35960564731c2d969e4a84abbd
daniel-fr0 <64498365+daniel-fr0@users.noreply.github.com>
2023-12-12 08:06:52 -0400

    concurrente

--- productoPunto.py
+++ productoPunto.py
@@ -1,20 +1,33 @@
+import time
 import multiprocessing
 
-def producto(x, y):
-	return x * y
-
 def productoPunto(a, b):
+	return sum([i * j for i, j in zip(a, b)])
+
+def productoPuntoConcurrente(a, b):
 	if len(a) != len(b):
 		print("Los vectores deben tener la misma longitud")
 		return None
+	
+	chunks = multiprocessing.cpu_count()
+	chunkSize = len(a) // chunks
 
-	with multiprocessing.Pool() as pool:
-		return sum(pool.starmap(producto, zip(a, b)))
+	with multiprocessing.Pool(processes = chunks) as pool:
+		result = pool.starmap(productoPunto, [(a[i:i + chunkSize], b[i:i + chunkSize]) for i in range(0, len(a), chunkSize)])
+		return sum(result)
+
 	
 if __name__ == '__main__':
 	N = 5_000_000
 	a = [i for i in range(N)]
 	b = [i for i in range(N)]
-	print(productoPunto(a, b))
 
-	# print(sum([i * j for i, j in zip(a, b)]))
+	start = time.time()
+	productoPuntoConcurrente(a, b)
+	end = time.time()
+	print("Concurrente:\t", end - start)
+
+	start = time.time()
+	productoPunto(a, b)
+	end = time.time()
+	print("No concurrente:\t", end - start)
